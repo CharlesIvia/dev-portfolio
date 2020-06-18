93fdf2d8ffcd8a33f7a332d3ab2d86ae861f2c7b
Charles Ivia <iviacharles@gmail.com>
2020-06-18 16:33:44 +0300

    Scroll reveal logic

---
+++ script.js
@@ -0,0 +1,16 @@
+//Scroll reveal
+
+window.sr = ScrollReveal();
+
+sr.reveal(".project-title", {
+  origin: "bottom",
+  duration: 1500,
+  distance: "25rem",
+  delay: 400,
+});
+sr.reveal(".project-title-two", {
+  origin: "bottom",
+  duration: 1500,
+  distance: "25rem",
+  delay: 400,
+});
