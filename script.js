96988d7ef22a20ecf748c97d11c3020e763b5267
Charles Ivia <iviacharles@gmail.com>
2020-06-20 14:01:15 +0300

    Initial portfolio animation

--- script.js
+++ script.js
@@ -8,8 +8,44 @@ sr.reveal(".project-title", {
   distance: "25rem",
   delay: 400,
 });
+sr.reveal(".skills-innerbox", {
+  origin: "bottom",
+  duration: 1500,
+  distance: "25rem",
+  delay: 300,
+});
 sr.reveal(".project-title-two", {
   origin: "bottom",
+  duration: 1500,
+  distance: "25rem",
+  delay: 400,
+});
+sr.reveal(".apple-small-div", {
+  origin: "left",
+  duration: 1500,
+  distance: "25rem",
+  delay: 400,
+});
+sr.reveal(".rosa-small-div", {
+  origin: "left",
+  duration: 1500,
+  distance: "25rem",
+  delay: 400,
+});
+sr.reveal(".budget-small-div", {
+  origin: "left",
+  duration: 1500,
+  distance: "25rem",
+  delay: 400,
+});
+sr.reveal(".apple-headline", {
+  origin: "right",
+  duration: 1500,
+  distance: "25rem",
+  delay: 300,
+});
+sr.reveal(".text-description", {
+  origin: "right",
   duration: 1500,
   distance: "25rem",
   delay: 400,
